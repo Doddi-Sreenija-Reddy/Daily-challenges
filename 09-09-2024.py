5f8ecb501993c3e76e95bad4704c3c914464ff37
Doddi Sreenija Reddy <128913265+Doddi-Sreenija-Reddy@users.noreply.github.com>
2025-04-21 01:14:14 +0530

    Update 09-09-2024.py

--- 09-09-2024.py
+++ 09-09-2024.py
@@ -6,8 +6,6 @@ def flag_sort(arr):
             low += 1
             mid += 1
         elif arr[mid] == 1:
-            #sreenija reddy
-            #ch.en.u4cse22016
             mid += 1
         else:
             arr[high], arr[mid] = arr[mid], arr[high]
